da872a2e9b6ef5f450046bdc279b6e30bd249ee4
Michael Gifford <michaelcgifford@gmail.com>
2016-02-19 12:44:23 -0500

    Data appearing in ListView

--- ListBooksView.js
+++ ListBooksView.js
@@ -6,13 +6,15 @@ var FAKE_DATA = [
 ];
  
 var React = require('react-native');
- 
+
 var {
     Image,
     StyleSheet,
     Text,
     View,
     Component,
+    ListView,
+    TouchableHighlight
    } = React;
  
 var styles = StyleSheet.create({
@@ -24,6 +26,10 @@ var styles = StyleSheet.create({
         backgroundColor: '#DCDCDC',
         padding: 10
     },
+    separator: {
+       height: 1,
+       backgroundColor: '#dddddd'
+   },
     thumbnail: {
         width: 53,
         height: 81,
@@ -42,17 +48,49 @@ var styles = StyleSheet.create({
 });
  
 class ListBooksView extends Component {
+
+    renderItem(item) {
+       return (
+            <TouchableHighlight>
+                <View>
+                    <View style={styles.container}>
+                        <Image
+                            source={{uri: item.volumeInfo.imageLinks.thumbnail}}
+                            style={styles.thumbnail} />
+                        <View style={styles.rightContainer}>
+                            <Text style={styles.title}>{item.volumeInfo.title}</Text>
+                            <Text style={styles.author}>{item.volumeInfo.authors}</Text>
+                        </View>
+                    </View>
+                    <View style={styles.separator} />
+                </View>
+            </TouchableHighlight>
+       );
+   }
+
+    constructor(props) {
+           super(props);
+           this.state = {
+               dataSource: new ListView.DataSource({
+                   rowHasChanged: (row1, row2) => row1 !== row2
+               })
+           };
+       }
+
+    componentDidMount() {
+        var items = FAKE_DATA;
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(items)
+        });
+       }   
+
     render() {
     var item = FAKE_DATA[0];
         return (
-            <View style={styles.container}>
-                <Image source={{uri: item.volumeInfo.imageLinks.thumbnail}}
-                            style={styles.thumbnail} />
-                <View style={styles.rightContainer}>
-                    <Text style={styles.title}>{item.volumeInfo.title}</Text>
-                    <Text style={styles.author}>{item.volumeInfo.authors}</Text>
-                </View>
-            </View>
+        <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderItem.bind(this)}
+            style={styles.listView}/>
         );
     }
 }
